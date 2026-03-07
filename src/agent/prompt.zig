cec26f182544e6d7cec0ab92795ed28acbbd6a57
qxo <49526356@qq.com>
2026-03-07 10:24:46 +0800

    fix(prompt): fix runtime os always macos issue

--- src/agent/prompt.zig
+++ src/agent/prompt.zig
@@ -290,7 +290,7 @@ pub fn buildSystemPrompt(
 
     // Runtime section
     try std.fmt.format(w, "## Runtime\n\nOS: {s} | Model: {s}\n\n", .{
-        @tagName(comptime std.Target.Os.Tag.macos),
+        @tagName(builtin.os.tag),
         ctx.model_name,
     });
 
